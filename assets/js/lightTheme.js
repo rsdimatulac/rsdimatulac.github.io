a44bbac3f0781de79dedfb4464a3c98d242a2773
Renerose Dimatulac <rsdimatulac@gmail.com>
2024-02-18 00:24:08 -0800

    Update theme switch, change color for logos

--- assets/js/lightTheme.js
+++ assets/js/lightTheme.js
@@ -8,12 +8,18 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const copyright = document.querySelector(".copyright");
     const input = document.querySelectorAll(".formfield");
     const tiles = document.querySelectorAll(".tile");
+    const sqlAlchemyLogo = document.querySelector(".devicon-sqlalchemy-plain-wordmark");
+    const sequelizeLogo = document.querySelector(".devicon-sequelize-plain-wordmark");
+    const cypressLogo = document.querySelector(".devicon-cypressio-plain-wordmark");
 
     themeSwitch.addEventListener("change", () => {
-        if (themeSwitch.checked) {
+        if (themeSwitch.checked) { // LIGHT THEME
             body.classList.add("white");
             signImage.src = "images/sign_black.png";
             copyright.classList.add("white");
+            sqlAlchemyLogo.classList.add("colored");
+            sequelizeLogo.classList.add("colored");
+            cypressLogo.classList.add("colored");
             
             aTag.forEach(tag => {
                 tag.classList.add("white");
@@ -22,17 +28,20 @@ window.addEventListener("DOMContentLoaded", (event) => {
             input.forEach(input => {
                 input.classList.add("white");
             });
-
+            
             tiles.forEach(tile => {
                 tile.classList.add("white");
             });
-
+            
             footer.style.backgroundColor = "rgba(246, 246, 246)";
             
-        } else {
+        } else { // DARK THEME
             body.classList.remove("white");
             signImage.src = "images/sign_white.png";
             copyright.classList.remove("white");
+            sqlAlchemyLogo.classList.remove("colored");
+            sequelizeLogo.classList.remove("colored");
+            cypressLogo.classList.remove("colored");
 
             aTag.forEach(tag => {
                 tag.classList.remove("white");
